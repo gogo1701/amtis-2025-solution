ad9ecb4bcd91af140f7e318d36198f74244d638c
Georgi <30569429+gogo1701@users.noreply.github.com>
2025-06-15 17:46:30 +0300

    Update Program.cs

--- MainApplication/Program.cs
+++ MainApplication/Program.cs
@@ -2,3 +2,6 @@
 
 MainApplication mainApplication = new();
 
+var application = await mainApplication.SessionService.GetSessionIdAsync(Models.Enums.SessionType.test);
+Console.WriteLine($"Session ID: {application.SessionId}");
+
